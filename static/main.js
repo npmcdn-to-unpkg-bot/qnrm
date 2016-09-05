15e96e63ba23085e966a9fdea3794938c0b10c85
Mor <mayowwwww@gmail.com>
2016-09-05 18:01:16 +0800

    o

--- static/main.js
+++ static/main.js
@@ -24,19 +24,31 @@ new Vue({
   el: '#app',
   data: function () {
     return {
+      waitListContainerVisual: false,
       list: [],
-      uploadList: []
+      uploadList: [],
+      root:{}
     }
   },
   ready() {
     this.loadList()
   },
   methods: {
+    /**
+     * 获取已上传的文件
+     */
     loadList: function () {
       this.$http.get('/file').then(function (response) {
         console.log(response)
-
+        response.data.items.forEach(function(item){
+          var path = item.key.split('/')
+          var temp_folder=this.root;
+          for(var i=0;i<path.length-1;i++){
+            
+          }
+        })
         for (var i = 0;i < response.data.items.length;i++) {
+          response.data.items[i].selected = false
           var flag = this.list.some(function (item) {
             return item.key == response.data.items[i].key
           })
@@ -48,20 +60,27 @@ new Vue({
       // this.$set('list', response.data.items)
       })
     },
+    /**
+     * 添加需要上传的文件
+     */
     addFile: function (event) {
       event.preventDefault()
       var files = event.dataTransfer.files
       for (var i = 0;i < files.length;i++) {
         var item = {
           complete: false,
-          status:'上传中...',
+          status: '上传中...',
           progress: 0,
           file: files[i]
         }
         this.startUpload(item)
         this.$data.uploadList.push(item)
+        this.showWaitListContainer()
       }
     },
+    /**
+     * 开始上传
+     */
     startUpload: function (item) {
       var file = item.file
       var formData = new FormData()
@@ -72,14 +91,74 @@ new Vue({
         console.log(progress)
       }).then(function (event) {
         item.complete = true
-        item.status='已完成'
+        item.status = '已完成'
         item.progress = 100
         self.loadList()
         console.log('done')
       })
     },
+    /**
+     * 切换选中状态
+     */
+    toggleSelectStatus: function (item) {
+      item.selected = !item.selected
+    },
+    /**
+     * 清楚所有项目的选中
+     */
+    clearSelectStatus:function(){
+      this.list.forEach(function(item){
+        item.selected = false
+      })
+    },
+    /**
+     * 全选 
+     */
+    selectAll:function(){
+      this.list.forEach(function(item){
+        item.selected = true
+      })
+    },
+    /**
+     * 反选
+     */
+    shiftSelectStatus:function(){
+      this.list.forEach(function(item){
+        item.selected = !item.selected 
+      })
+    },
+    
+    /**
+     * 显示上传界面
+     */
+    showWaitListContainer: function () {
+      this.waitListContainerVisual = true
+    },
+    /**
+     * 切换上传界面
+     */
+    toggleWaitListContainer: function () {
+      this.waitListContainerVisual = !this.waitListContainerVisual
+    },
+    /**
+     * 阻止默认事件
+     */
     preventDefault: function (event) {
       event.preventDefault()
+    }
+  },
+  computed: {
+    waitUploadListCount: function () {
+      var list = this.uploadList.filter(function (item) {
+        return item.complete === false
+      })
+      return list.length
+    },
+    selectedListCount:function(){
+      var list = this.list.filter(function (item) {
+        return item.selected === true
+      })
+      return list.length
     }
   }
 })
